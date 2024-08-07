edd9bde24c6d9b99059af019db9e085f78fd731f
bmehdi777 <mehdi.bentouati76@gmail.com>
2024-08-08 01:05:24 +0200

    adding args

--- pkg/client/cli.go
+++ pkg/client/cli.go
@@ -1,7 +1,9 @@
 package client
 
 import (
+	"errors"
 	"log"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -23,9 +25,16 @@ func Execute() {
 	}
 
 	var httpCmd = &cobra.Command{
-		Use:   "http",
+		Use:   "http <PORT>",
 		Short: "Proxyfy http request",
-		Args:  cobra.MatchAll(cobra.MinimumNArgs(1), cobra.MaximumNArgs(1)),
+		Args: cobra.MatchAll(cobra.MinimumNArgs(1), cobra.MaximumNArgs(1), func(cmd *cobra.Command, arg []string) error {
+			if value, err := strconv.Atoi(arg[0]); err != nil {
+				return errors.New("PORT should be a numeric value")
+			} else if value <= 0 || value > 65535 {
+				return errors.New("PORT should be between 0 and 65535")
+			}
+			return nil
+		}),
 		Run: func(cmd *cobra.Command, arg []string) {
 			log.Println("open port !")
 			log.Println("args : ", arg)
